9ae508acad60e3c9266c62b2438bfd6134892b86
Candyzorua <jiaying.ong2@gmail.com>
2026-01-28 20:41:00 -0800

    infix operator, partial function application

--- cs3110/chapter2.ml
+++ cs3110/chapter2.ml
@@ -3,17 +3,27 @@ let rec fib = function
   | 1 -> 1
   | x -> fib (x - 1) + fib (x - 2)
 
+(* auxilliary functions can be defined *)
 let fib_fast n = 
-  let rec aux iters_remaining prev double_prev = match iters_remaining with
+  let rec aux iters_remaining double_prev prev = match iters_remaining with
     | iters_remaining when iters_remaining <= 0 -> 0
     | iters_remaining when iters_remaining = 1 -> prev
-    | iters_remaining -> aux (iters_remaining-1) (prev + double_prev) (prev)
-  in aux n 1 0
+    | iters_remaining -> aux (iters_remaining-1) (prev) (prev + double_prev) 
+  in aux n 0 1
+
+(* infix operators can be defined *)
+let ( +/. ) a b = (a +. b) /. 2.
+
+(* add is a curried function so partial application returns another function *)
+let add x y = x + y
 
 let () =
-  print_endline (string_of_int (fib 5));
-  print_endline (string_of_int (fib_fast 5));
-  assert ((fib 5) = (fib_fast 5))
+  assert ((fib 0) = (fib_fast 0));
+  assert ((fib 1) = (fib_fast 1));
+  assert ((fib 2) = (fib_fast 2));
+  assert ((fib 5) = (fib_fast 5));
+  assert (3. +/. 7. = 5.);
+  assert ((add 5) 1  = add 5 1)
   
 
     
