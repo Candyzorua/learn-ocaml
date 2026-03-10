ee91b1cd42e5e0e6780509acb280355ec5131a15
Candyzorua <jiaying.ong2@gmail.com>
2026-03-09 19:13:23 -0700

    rwo: fix dll

--- real_world_ocaml/lib/doubly_linked_list.ml
+++ real_world_ocaml/lib/doubly_linked_list.ml
@@ -6,33 +6,56 @@ type 'a element =
   ; mutable prev : 'a element option
   }
 
-type 'a t = 'a element option ref
+type 'a t =
+  { mutable first : 'a element option
+  }
+
+let create () = { first = None }
+let is_empty t = Option.is_none t.first
+let first t = t.first
+let next elt = elt.next
+let prev elt = elt.prev
+let value elt = elt.value
+
+let iter t ~f =
+  let rec loop = function
+    | None -> ()
+    | Some elt ->
+      f elt.value;
+      loop elt.next
+  in
+  loop t.first
+
+let find_el t ~f =
+  let rec loop = function
+    | None -> None
+    | Some elt when f elt.value -> Some elt
+    | Some elt -> loop elt.next
+  in
+  loop t.first
 
 let insert_first t value =
-  let new_elt = { prev = None; next = !t; value } in
-  (match !t with
-  | Some old_first -> old_first.prev <- Some new_elt
-  | None -> ());
-  t := Some new_elt;
+  let new_elt = { value; prev = None; next = t.first } in
+  (match t.first with
+   | Some old_first -> old_first.prev <- Some new_elt
+   | None -> ());
+  t.first <- Some new_elt;
   new_elt
 
 let insert_after elt value =
   let new_elt = { value; prev = Some elt; next = elt.next } in
   (match elt.next with
-  | Some old_next -> old_next.prev <- Some new_elt
-  | None -> ());
+   | Some old_next -> old_next.prev <- Some new_elt
+   | None -> ());
   elt.next <- Some new_elt;
   new_elt
 
 let remove t elt =
-  let { prev; next; _ } = elt in 
-    (match prev with 
-    | Some prev -> prev.next <- next
-    | None -> t := next);
-    (match next with 
-    | Some next -> next.prev <- prev
-    | None -> ());
-  elt.prev<-None;
-  elt.next<-None
-
-  
+  (match elt.prev with
+   | Some prev -> prev.next <- elt.next
+   | None -> t.first <- elt.next);
+  (match elt.next with
+   | Some next -> next.prev <- elt.prev
+   | None -> ());
+  elt.prev <- None;
+  elt.next <- None
